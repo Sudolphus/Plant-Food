039a0a2759d44703808e85a76bf30b7451151dd4
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-01 11:16:12 -0700

    match homework state
    Co-authored-by: Zentraxius <clanalia55@gmail.com>
    Co-authored-by: quietevolver <quietevolver99@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { stateControl, blueFood } from './plant.js';
+
+$(document).ready(function() {
+  $('#feed').click(function() {
+    const newState = stateControl(blueFood);
+    $(`#soil-value`).text(`Soil: ${newState.soil}`);
+  });
+
+  $('#show-state').click(function() {
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  });
+});
---
+++ src/plant.js
@@ -0,0 +1,24 @@
+export const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  };
+};
+
+export const stateControl = storeState();
+
+export const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    });
+  };
+};
+
+export const feed = changeState("soil")(1);
+export const blueFood = changeState("soil")(5);
+export const hydrate = changeState("water")(1);
+export const superHydrate = changeState("water")(5);
