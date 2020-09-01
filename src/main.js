971b6387b311db96e64bf885e6ac03656dceb666
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-01 14:37:33 -0700

    add additional plants
    Co-authored-by: Zentraxius <clanalia55@gmail.com>
    Co-Authored-By: Vera Weikal <quietevolver99@gmail.com>

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import { storeState, changeState } from './plant';
 $(document).ready(function() {
   const plant = storeState(); //plant = {soil: 0, water: 0}
 
+  
+  
+  const plantTwo = storeState();
+  const plantThree = storeState();
+  
   $('#feed-plant').click(function() {
     plant(changeState("soil")(1));
     $('#plant-soil-value').html(plant().soil);
@@ -20,5 +25,35 @@ $(document).ready(function() {
   $('#light-plant').click(function() { 
     plant(changeState("light")(5));
     $('#plant-light-value').html(plant().light);
+  });
+
+  $('#feed-plant-two').click(function() {
+    plantTwo(changeState("soil")(1));
+    $('#plant-soil-value-two').html(plantTwo().soil);
+  });
+
+  $('#water-plant-two').click(function() {
+    plantTwo(changeState("water")(1));
+    $('#plant-water-value-two').html(plantTwo().water);
+  });
+
+  $('#light-plant-two').click(function() { 
+    plantTwo(changeState("light")(5));
+    $('#plant-light-value-two').html(plantTwo().light);
+  });
+  
+  $('#feed-plant-three').click(function() {
+    plantThree(changeState("soil")(1));
+    $('#plant-soil-value-three').html(plantThree().soil);
+  });
+
+  $('#water-plant-three').click(function() {
+    plantThree(changeState("water")(1));
+    $('#plant-water-value-three').html(plantThree().water);
+  });
+
+  $('#light-plant-three').click(function() { 
+    plantThree(changeState("light")(5));
+    $('#plant-light-value-three').html(plantThree().light);
   });
 });
